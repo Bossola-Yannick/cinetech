dbcb2d7b6e0623a0f04cb2c222542112032bea07
James Sanchez <james.sanchez@laplateforme.io>
2025-04-15 22:54:20 +0200

    feat: added arrow for slide navigation

--- js/index.js
+++ js/index.js
@@ -1,14 +1,37 @@
+const rightArrow = document.querySelector(".arrows-right");
+// const leftArrow = document.querySelector(".arrows-left");
+
 async function getTrendingMovies() {
   const trendingMovies = `https://api.themoviedb.org/3/trending/movie/week?language=fr-FR`;
   let data = await getData(trendingMovies);
+
+  // création des affiches
   if (data && data.results) {
-    console.log(data);
     data.results.forEach((movie) => {
-      //   console.log(movie.id);
       makingCard(movie.poster_path, movie.title, movie.id);
     });
+
+    // carrousel
+    const movieLists = document.getElementById("container-movies");
+    const movie = movieLists.querySelectorAll("article").length;
+
+    let click = 0;
+    rightArrow.addEventListener("click", () => {
+      const ratio = Math.floor(window.innerWidth / 302);
+      click++;
+
+      if (movie - ratio * click >= 0) {
+        movieLists.style.transform = `translateX(${
+          movieLists.computedStyleMap().get("transform")[0].x.value -
+          302 * ratio
+        }px)`;
+      } else {
+        movieLists.style.transform = "translateX(0)";
+        click = 0;
+      }
+    });
   } else {
-    console.error("Les données retournées ne contiennent pas de films.");
+    console.error("aucun film trouvé");
   }
 }
 
