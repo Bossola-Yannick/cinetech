449eee2243786834c10c056c95246b857a94ac1e
James Sanchez <james.sanchez@laplateforme.io>
2025-04-16 13:01:39 +0200

    feat: working slider for trending movies

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
 const rightArrow = document.querySelector(".arrows-right");
-// const leftArrow = document.querySelector(".arrows-left");
+const leftArrow = document.querySelector(".arrows-left");
 
 async function getTrendingMovies() {
   const trendingMovies = `https://api.themoviedb.org/3/trending/movie/week?language=fr-FR`;
@@ -8,26 +8,70 @@ async function getTrendingMovies() {
   // création des affiches
   if (data && data.results) {
     data.results.forEach((movie) => {
-      makingCard(movie.poster_path, movie.title, movie.id);
+      makingCard(movie.poster_path, movie.title, movie.id, "movies");
     });
 
     // carrousel
     const movieLists = document.getElementById("container-movies");
-    const movie = movieLists.querySelectorAll("article").length;
+    const movieLenght = movieLists.querySelectorAll("article").length;
+    const ratio = Math.floor(window.innerWidth / 302);
+
+    // copie une partie des films pour un affichage "complet"
+    let movieRatio = movieLenght % ratio;
+    const moviesCopy = data.results.slice(0, ratio - movieRatio);
+    if (movieRatio !== 0) {
+      moviesCopy.forEach((copy) => {
+        makingCard(copy.poster_path, copy.title, copy.id, "movies");
+      });
+    }
+
+    const newLenght = movieLists.querySelectorAll("article").length;
 
     let click = 0;
-    rightArrow.addEventListener("click", () => {
-      const ratio = Math.floor(window.innerWidth / 302);
-      click++;
 
-      if (movie - ratio * click >= 0) {
+    rightArrow.addEventListener("click", () => {
+      click++;
+      console.log(newLenght - ratio * click);
+      // affichage fleche gauche
+      if (click >= 1) {
+        leftArrow.style.visibility = "visible";
+      }
+
+      // gestion click fleche droite
+
+      if (newLenght - ratio * click > 0) {
+        if (newLenght - ratio * click === ratio) {
+          rightArrow.style.visibility = "hidden";
+          movieLists.style.transform = `translateX(${
+            movieLists.computedStyleMap().get("transform")[0].x.value -
+            302 * ratio
+          }px)`;
+        } else {
+          rightArrow.style.visibility = "visible";
+          movieLists.style.transform = `translateX(${
+            movieLists.computedStyleMap().get("transform")[0].x.value -
+            302 * ratio
+          }px)`;
+        }
+      } else {
+        leftArrow.style.visibility = "hidden";
+        click = 0;
+      }
+    });
+
+    // gestion fleche gauche
+    leftArrow.addEventListener("click", () => {
+      if (newLenght - ratio * click > 0) {
+        click--;
+        rightArrow.style.visibility = "visible";
         movieLists.style.transform = `translateX(${
-          movieLists.computedStyleMap().get("transform")[0].x.value -
+          movieLists.computedStyleMap().get("transform")[0].x.value +
           302 * ratio
         }px)`;
-      } else {
-        movieLists.style.transform = "translateX(0)";
-        click = 0;
+
+        if (click === 0) {
+          leftArrow.style.visibility = "hidden";
+        }
       }
     });
   } else {
